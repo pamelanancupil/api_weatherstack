2ded6631fea569d41f01a3a535e7cb982221e091
pamelanancupil <pamela.nancupilv@gmail.com>
2020-10-16 22:01:21 -0300

    imputs y css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,16 +9,41 @@ import { WeatherService } from '../../services/weather.service'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private weatherService: WeatherService) {
+  weather;
+  
 
+  constructor(private weatherService: WeatherService) {
+    
    }
 
   ngOnInit(): void {
-    this.weatherService.getWeather('santiago','cl')
+  
+  }
+
+  getWeather(cityName:string, countryName:string){
+    this.weatherService.getWeather(cityName, countryName)
     .subscribe(
-      res=> console.log(res),
-      err=> console.log(err)
+      res => { console.log(res);
+             this.weather = res
+            },
+      err => console.log(err)
     )
+
+  }
+
+  submitLocation(cityName, countryName){
+    this.getWeather(cityName.value, countryName.value);
+
+    console.log(cityName.value, countryName.value);
+    cityName.value= '';
+    countryName.value = '';
+    cityName.focus();
+    return false;
+  }
+
+
+  cambio(e){
+    
   }
 
 }
